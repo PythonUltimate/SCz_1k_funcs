8f034509c0c07a4c30d805dce7fb182ea30767d1
SlawCzech <slaw.czech@gmail.com>
2022-11-09 17:22:05 +0100

    basic maths

---
+++ count_funcs.py
@@ -0,0 +1,14 @@
+
+files = ['funcs_100.py']
+
+counter = 0
+
+for file in files:
+    with open(file) as file_:
+        data = file_.read()
+        occurences = data.count('def')
+        counter += occurences
+        print(f'Number of functions in {file}: {occurences}')
+
+print(f'Functions total: {counter}')
+
