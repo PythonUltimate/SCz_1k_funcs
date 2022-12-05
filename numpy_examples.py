003cd76ef76519366af07a243a7de8f8488ff5a4
SlawCzech <slaw.czech@gmail.com>
2022-12-05 22:07:48 +0100

    numpy update

--- numpy_examples.py
+++ numpy_examples.py
@@ -41,7 +41,6 @@ def check_dimensions(fn, array, array2, array3, array4):
     return elo.ndim
 
 
-
 # heja = check_dimensions(create_3D_array, test_list, test_list_2, test_list_3, test_list_4)
 
 def check_numpy_datatype(array):
@@ -91,6 +90,7 @@ def check_array_shape(array, array2):
     arr = np.array([array, array2])
     return arr.shape
 
+
 def check_nested_array_shape(array, num):
     arr = np.array(array, ndmin=num)
     return arr.shape
@@ -102,12 +102,197 @@ def reshape_array(array, num1, num2):
     return newarr
 
 
-def check_if_reshape_is_view(array, num1, num2): # tak, reshaping daje view!!!
+def check_if_reshape_is_view(array, num1, num2):  # tak, reshaping daje view!!!
     arr = np.array(array)
     newarr = arr.reshape(num1, num2)
     return newarr.base
 
-n = check_if_reshape_is_view(test_list, 10, 1)
+
+def make_with_unknown_dimension(array, num):
+    arr = np.array(array)
+    return arr.reshape(num, -1)
+
+
+def convert_to_1D_array(array, array2, array3, array4):
+    arr = np.array(([array, array2], [array3, array4]))
+    return arr.reshape(-1)
+
+
+def iterate_by_elems(array):
+    for elem in np.array(array):
+        print(elem)
+
+
+def iterate_by_rows(array, array2):
+    for elem in np.array([array, array2]):
+        print(elem)
+
+
+def print_rows_of_3D_array(array, array2, array3, array4):
+    for elem in np.array([[array, array2], [array3, array4]]):
+        print(elem)
+
+
+def print_nestedrows_of_3D_array(array, array2, array3, array4):
+    for row in np.array([[array, array2], [array3, array4]]):
+        for elem in row:
+            print(elem)
+
+
+def print_scalars_of_3D_array(array, array2, array3, array4):
+    for row in np.array([[array, array2], [array3, array4]]):
+        for elem in row:
+            for scalar in elem:
+                print(scalar)
+
+
+def iterate_with_nditer(array, array2, array3, array4):
+    arr = np.array([[array, array2], [array3, array4]])
+    for elem in np.nditer(arr):
+        print(elem)
+
+
+def iterate_as_string(array):
+    arr = np.array(array)
+    for elem in np.nditer(arr, flags=['buffered'], op_dtypes=['S']):
+        print(elem)
+
+
+def iterate_with_skip_2(array):
+    arr = np.array(array)
+    for x in np.nditer(arr[::2]):
+        print(x)
+
+
+def print_with_enumeration(array):
+    arr = np.array(array)
+    for idx, x in np.ndenumerate(arr):
+        print(idx, x)
+
+
+def print_3D_with_enumeration(array, array2, array3, array4):
+    arr = np.array([[array, array2], [array3, array4]])
+    for idx, x in np.ndenumerate(arr):
+        print(idx, x)
+
+
+def concatenate_arrays(array, array2):
+    arr1 = np.array(array)
+    arr2 = np.array(array2)
+    new_arr = np.concatenate((arr1, arr2))
+    return new_arr
+
+
+def concatenate_on_axis(array, array2, array3, array4, axis):
+    arr1 = np.array([array, array2])
+    arr2 = np.array([array3, array4])
+    new_arr = np.concatenate((arr1, arr2), axis=axis)
+    return new_arr
+
+
+def join_by_stacking(array, array2, axis):
+    arr1 = np.array(array)
+    arr2 = np.array(array2)
+    new_arr = np.stack((arr1, arr2), axis=axis)
+    return new_arr
+
+
+def join_by_rows_stacking(array, array2):
+    arr1 = np.array(array)
+    arr2 = np.array(array2)
+    new_arr = np.hstack((arr1, arr2))
+    return new_arr
+
+
+def join_by_columns_stacking(array, array2):
+    arr1 = np.array(array)
+    arr2 = np.array(array2)
+    new_arr = np.vstack((arr1, arr2))
+    return new_arr
+
+
+def join_by_depth_stacking(array, array2):
+    arr1 = np.array(array)
+    arr2 = np.array(array2)
+    new_arr = np.dstack((arr1, arr2))
+    return new_arr
+
+
+def split_array(array, num):
+    arr = np.array(array)
+    new_arr = np.split(arr, num)
+    return new_arr
+
+
+def adjusted_split_array(array, num):
+    arr = np.array(array)
+    new_arr = np.array_split(arr, num)
+    return new_arr
+
+
+def adjusted_split_nD_array(array, array2, array3, array4, num):
+    arr = np.array([[array, array2], [array3, array4]])
+    new_arr = np.array_split(arr, num)
+    return new_arr
+
+
+def find_index_of_value(array, value):
+    arr = np.array(array)
+    idx = np.where(arr == value)
+    return idx
+
+
+def find_even_numbers(array):
+    arr = np.array(array)
+    idx = np.where(arr % 2 == 0)
+    return idx
+
+
+def find_odd_numbers(array):
+    arr = np.array(array)
+    idx = np.where(arr % 2 != 0)
+    return idx
+
+
+def find_higher_than(array, num):
+    arr = np.array(array)
+    idx = np.where(arr > num)
+    return idx
+
+
+def sort_array(array):
+    arr = np.array(array)
+    return np.sort(arr)
+
+
+def sort_nD_array(array, array2, array3, array4):
+    arr = np.array([[array, array2], [array3, array4]])
+    return np.sort(arr)
+
+
+def sort_string_list(str_array):
+    arr = np.array(str_array)
+    return np.sort(arr)
+
+def search_where_insert(array, num):
+    arr = np.array(array)
+    sorted_array = np.sort(arr)
+    print(np.searchsorted(sorted_array, num))
+
+
+def search_where_insert_from_right(array, num):
+    arr = np.array(array)
+    sorted_array = np.sort(arr)
+    print(np.searchsorted(sorted_array, num, side='right'))
+
+
+def search_where_multiple_insert(array, num, num2, num3):
+    arr = np.array(array)
+    sorted_array = np.sort(arr)
+    print(np.searchsorted(sorted_array, [num, num2, num3]))
+
+
+n = sort_string_list(str_list)
 
 print(n)
 print(type(n))
